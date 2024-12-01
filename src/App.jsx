96f2c6ac2155d017c3801cbdc9a88bdd28492f7d
elshora97 <A.elshora@dataserve.com.sa>
2024-12-02 01:32:58 +0200

    Add chat message input and actions

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <div className="container">
       <List />
-      <Detail />
       <Chat />
+      <Detail />
     </div>
   );
 };
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,86 @@
+import { useState } from "react";
 import "./chat.css";
-
+import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [openEmoji, setOpenEmoji] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpenEmoji(false);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img
+            src="/public/avatar.png"
+            alt=""
+          />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img
+            src="/public/phone.png"
+            alt=""
+          />
+          <img
+            src="/public/video.png"
+            alt=""
+          />
+          <img
+            src="/public/info.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img
+            src="/public/img.png"
+            alt="img"
+          />
+          <img
+            src="/public/camera.png"
+            alt="camera"
+          />
+          <img
+            src="/public/mic.png"
+            alt="mic"
+          />
+        </div>
+
+        <input
+          type="text"
+          placeholder="type your message..."
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          value={text}
+        />
+
+        <div className="emoji">
+          <img
+            src="/public/emoji.png"
+            alt="emoji"
+            onClick={() => setOpenEmoji((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker
+              open={openEmoji}
+              onEmojiClick={handleEmoji}
+            />
+          </div>
+        </div>
+        <button className="sendBtn">send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
