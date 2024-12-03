e8b90afb8348600c4f0ee9f487b51a44380da037
elshora97 <A.elshora@dataserve.com.sa>
2024-12-03 05:54:11 +0200

    add userStore using zustand

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userstore";
 
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
     });
 
     return () => {
-      unsub();
+      unSub();
     };
-  }, []);
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
